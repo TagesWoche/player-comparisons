52215407227940d74ec93210bfe29947b28ee76a
David Bauer <info@davidbauer.ch>
2013-06-25 15:11:35 +0200

    Files added

---
+++ script.js
@@ -0,0 +1,42 @@
+$(function() {
+	
+	
+	$('#searchform').submit(function(e) {
+		// Stop the form from sending and reloading the page
+		e.preventDefault();
+		$('#error').html("");
+
+		// Find the players
+		var player1 = findPlayers(1);
+		var player2 = findPlayers(2);
+		generateScorecard(player1,player2);		
+	});
+});
+
+
+
+
+// store username given via input
+function findPlayers(num) {
+
+		var player;
+		var id = "player"+num;
+		console.log(id);
+		// Get the player value from the form dropdown
+		player = document.getElementById(id).value;
+		
+		console.log(player);
+		
+		return player;		
+}
+
+
+// generate scorecards
+function generateScorecard(player1,player2) {
+	$('.player1').html('<iframe src="http://labs.tageswoche.ch/scorecards/scorecard.html?spieler=' + player1 + '" width="330" height="620"></iframe>');
+	$('.player2').html('<iframe src="http://labs.tageswoche.ch/scorecards/scorecard.html?spieler=' + player2 + '" width="330" height="620"></iframe>');
+	
+}
+l?spieler=' + player2 + '" width="330" height="620"></iframe>');
+	
+}
